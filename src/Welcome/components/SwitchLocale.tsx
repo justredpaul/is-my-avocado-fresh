23799efaff51910fbaca2d95c06a4615597a8594
justredpaul <justredpaul@yandex.ru>
2022-05-22 03:19:26 +0300

    Add style for locale switcher

--- src/Welcome/components/SwitchLocale.tsx
+++ src/Welcome/components/SwitchLocale.tsx
@@ -2,20 +2,33 @@ import * as React from 'react';
 import { useStore } from '@nanostores/react';
 
 import { styled } from '../../../stitches.config';
-import { localeState } from '../../Common/stores/locale.store';
+import { localeState, TLocale } from '../../Common/stores/locale.store';
 import { AVAILABLE_LOCALES, LOCALE_NAMES } from '../../Common/constants/i18n';
 
 const Select = styled('select', {
+    appearance: 'none',
+
     position: 'absolute',
     top: '$s4',
     right: '$s4',
-    border: '2px solid blue',
+
+    padding: '$s2 $s5',
+
+    border: '4px solid $blue',
+    borderRadius: '$r2',
+    backgroundColor: 'transparent',
+    color: '$white70',
+    fontSize: '$fz1',
+    '&:focus': {
+        outline: 'none',
+        borderColor: '$primary',
+    }
 });
 
 export const SwitchLocale = () => {
     const locale = useStore(localeState);
 
-    const onChange = ({ target: { value: nextLocale }}) => localeState.set(nextLocale);
+    const onChange = ({ target: { value: nextLocale }}: { target: { value: TLocale }}) => localeState.set(nextLocale);
 
     return (
         <Select value={locale} onChange={onChange}>
